f0aeb8d367df5abe7446d15952733ccdad7d20f6
Mike Donovan <mike@mikedonovan.dev>
2023-02-06 14:36:43 -0800

    Create basic question display functionality

--- src/index.js
+++ src/index.js
@@ -1,79 +1,135 @@
 import './css/styles.css';
 
-const states = { AL: 'Alabama', AK: 'Alaska', AZ: 'Arizona', AR: 'Arkansas', CA: 'California', CO: 'Colorado', CT: 'Connecticut', DE: 'Delaware', FL: 'Florida', GA: 'Georgia', HI: 'Hawaii', ID: 'Idaho', IL: 'Illinois', IN: 'Indiana', IA: 'Iowa', KS: 'Kansas', KY: 'Kentucky', LA: 'Louisiana', ME: 'Maine', MD: 'Maryland', MA: 'Massachusetts', MI: 'Michigan', MN: 'Minnesota', MS: 'Mississippi', MO: 'Missouri', MT: 'Montana', NE: 'Nebraska', NV: 'Nevada', NH: 'New Hampshire', NJ: 'New Jersey', NM: 'New Mexico', NY: 'New York', NC: 'North Carolina', ND: 'North Dakota', OH: 'Ohio', OK: 'Oklahoma', OR: 'Oregon', PA: 'Pennsylvania', RI: 'Rhode Island', SC: 'South Carolina', SD: 'South Dakota', TN: 'Tennessee', TX: 'Texas', UT: 'Utah', VT: 'Vermont', VA: 'Virginia', WA: 'Washington', WV: 'West Virginia', WI: 'Wisconsin', WY: 'Wyoming' };
+let sessionKey = '';
+let categories = [];
+getSessionKey();
+getCategories();
 
-function fillStateDropdown() {
-  for (const abbrev in states) {
-    document.getElementById('state-name-input').innerHTML += `
-      <option name=${states[abbrev]}>${abbrev}</option>
-    `;
-  }
-}
+let questionList;
 
-const apiKey = process.env.API_KEY;
-
-function getWeather(city, state, units) {
-  let geoRequest = new XMLHttpRequest();
-  const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state}}&limit=5&appid=${apiKey}`;
-  geoRequest.addEventListener("loadend", function () {
+function getSessionKey() {
+  let tokenRequest = new XMLHttpRequest();
+  const url = `https://opentdb.com/api_token.php?command=request`;
+  tokenRequest.addEventListener("loadend", function () {
     let response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      let targetCity = response.filter(obj => obj.state === state)[0];
-      let lat = targetCity.lat;
-      let lon = targetCity.lon;
-      let unitType = 'metric';
-      if (units === 'Fahrenheit') {
-        unitType = 'imperial';
-      } else if (units === 'Kelvin') {
-        unitType = 'standard';
-      }
-      let request = new XMLHttpRequest();
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unitType}`;
-      request.addEventListener("loadend", function () {
-        let response = JSON.parse(this.responseText);
-        document.getElementById('loading-message').classList.remove('showing');
-        if (this.status === 200) {
-          printWeatherInfo(city, state, units, response);
-        } else {
-          document.getElementById('output-area').innerHTML = `
-            <div style="color: red">ERROR ${this.status}: ${this.statusText}</div>
-          `;
-        }
-      });
-
-      request.open("GET", url, true);
-      request.send();
-    } else {
-      document.getElementById('loading-message').classList.remove('showing');
-      document.getElementById('output-area').innerHTML = `
-        <div style="color: red">ERROR ${this.status}: ${this.statusText}</div>
+    sessionKey = response.token;
+  });
+  tokenRequest.open("GET", url, true);
+  tokenRequest.send();
+}
+function getCategories() {
+  let catRequest = new XMLHttpRequest();
+  const url = `https://opentdb.com/api_category.php`;
+  catRequest.addEventListener("loadend", function () {
+    let response = JSON.parse(this.responseText);
+    categories = response.trivia_categories;
+    for (const obj of categories) {
+      document.getElementById('subject-select').innerHTML += `
+        <option name=${obj.id}>${obj.name}</option>
       `;
     }
   });
-
-  geoRequest.open("GET", geoUrl, true);
-  geoRequest.send();
+  catRequest.open("GET", url, true);
+  catRequest.send();
 }
 
-function printWeatherInfo(city, state, units, data) {
-  document.getElementById('output-area').innerHTML = `
-    <img src='http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'>
-    <h2>${city[0].toUpperCase() + city.substring(1, city.length)}, ${state}</h2>
-    <div>${data.weather[0].main}</div>
-    <div>Temp: ${Math.round(data.main.temp)}° ${units}</div>
-    <div>Humidity: ${data.main.humidity}%</div>
-  `;
+document.getElementById('trivia-form').addEventListener('submit', handleStartGameClick);
+
+function getIDForCategory(categoryName) {
+  for (const obj of categories) {
+    if (obj.name === categoryName) {
+      return obj.id;
+    }
+  }
 }
 
-document.getElementById('location-form').addEventListener('submit', (e) => {
+async function createTriviaCards() {
+  // document.getElementById('card-area').innerHTML = `<form id="answers-form">`;
+  let answersForm = document.createElement('form');
+  answersForm.id = 'answers-form';
+  for (const questionObj of questionList) {
+    let cardObj = document.createElement('div');
+    cardObj.classList.add('trivia-card');
+    let choicesHTML = '';
+    if (questionObj.type === 'boolean') {
+      choicesHTML = `
+        <div>
+          <input type="radio" id="true-${questionList.indexOf(questionObj)}" name="boolean" value="true">
+          <label for="true-${questionList.indexOf(questionObj)}">True</label>
+        </div>
+        <div>
+          <input type="radio" id="false-${questionList.indexOf(questionObj)}" name="boolean" value="false">
+          <label for="false-${questionList.indexOf(questionObj)}">False</label>
+        </div>
+      `;
+    } else {
+      // make array of answers with correct at random index
+
+      let totalChoices = questionObj.incorrect_answers.length + 1;
+      let correctIndex = randomInt(0, totalChoices);
+      let choiceArray = [...questionObj.incorrect_answers];
+      choiceArray.splice(correctIndex, 0, questionObj.correct_answer);
+      for (const choice of choiceArray) {
+        choicesHTML += `
+          <div>
+            <input type="radio" id="${choice}" name="${questionObj.question}" value="choice">
+            <label for="${choice}">${choice}</label>
+          </div>
+        `;
+      }
+    }
+    cardObj.innerHTML = `
+      <h4>${questionObj.category}</h4>
+      <div class="question">${questionObj.question}</div>
+      ${choicesHTML}
+    `;
+
+    document.getElementById('card-area').append(answersForm);
+    answersForm.append(cardObj);
+    await pause(100);
+    cardObj.classList.add('showing');
+  }
+}
+
+async function handleStartGameClick(e) {
   e.preventDefault();
-  document.getElementById('output-area').innerHTML = ``;
-  document.getElementById('loading-message').classList.add('showing');
-  let userCity = document.getElementById('city-name-input').value;
-  let userState = states[document.getElementById('state-name-input').value];
-  let userUnits = document.getElementById('temp-units-input').value;
-  console.log('getting weather for', userCity, userState);
-  getWeather(userCity, userState, userUnits);
-});
+  document.getElementById('card-area').innerHTML = '';
+  document.getElementById('trivia-form').classList.add('working');
+  questionList = [];
+  let request = new XMLHttpRequest();
+  let questionCount = document.getElementById('question-count').value;
+  let subjectID = getIDForCategory(document.getElementById('subject-select').value);
+  let difficulty = document.getElementById('difficulty-select').value.toLowerCase();
+  const url = `https://opentdb.com/api.php?amount=${questionCount}&category=${subjectID}&difficulty=${difficulty}`;
+  request.addEventListener("loadend", async function () {
+    let response = JSON.parse(this.responseText);
+    if (response.results.length) {
+      document.getElementById('trivia-form').classList.remove('working');
+      questionList = response.results;
+      await createTriviaCards();
+      createCheckAnswersButton();
+    }
+  });
 
-fillStateDropdown();
+  request.open("GET", url, true);
+  request.send();
+}
+
+function createCheckAnswersButton() {
+  document.getElementById('answers-form').innerHTML += `
+    <div class="button-end">
+      <button type="submit">Check Answers</button>
+    </div>
+  `;
+  document.getElementById('answers-form').addEventListener('submit', handleCheckAnswersClick);
+}
+
+function handleCheckAnswersClick(e) {
+  e.preventDefault();
+  for (const el of document.getElementsByClassName('trivia-card')) {
+    console.log('checking el', el)
+  }
+}
+
+const randomInt = (min, max) => Math.round(Math.random() * (max - min) + min);
+const pause = async ms => new Promise(resolve => setTimeout(resolve, ms));
